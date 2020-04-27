de3c76f9bf52f7e5218eeb0baa10db663ad233f5
github username <thumphrey9123@gmail.com>
2020-04-27 18:48:10 -0400

    fixed jewels and stones

--- src/JewelsAndStones.js
+++ src/JewelsAndStones.js
@@ -1,13 +1,3 @@
-// Worst case O(n) and best case O(log n)
-var numJewelsInStones = function(J, S) {
-    let count = 0
-    for(let i=0;i<S.length;i++){
-        if(J.includes(S[i])){
-            count++
-        }
-    }
-    return count
-};
 
 // O(n)time & O(n) time
 var numJewelsInStones = function(J, S) {
@@ -18,6 +8,17 @@ var numJewelsInStones = function(J, S) {
     }
     for(let i=0;i<S.length;i++){
         if(dp[S[i]]){
+            count++
+        }
+    }
+    return count
+};
+
+// Worst case O(n) and best case O(log n)
+var numJewelsInStones = function(J, S) {
+    let count = 0
+    for(let i=0;i<S.length;i++){
+        if(J.includes(S[i])){
             count++
         }
     }
