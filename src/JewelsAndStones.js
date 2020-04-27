422019afc2bb7a9862256303cb3603a87242662e
github username <thumphrey9123@gmail.com>
2020-04-27 18:47:35 -0400

    Jewels and stones 2

--- src/JewelsAndStones.js
+++ src/JewelsAndStones.js
@@ -1,3 +1,4 @@
+// Worst case O(n) and best case O(log n)
 var numJewelsInStones = function(J, S) {
     let count = 0
     for(let i=0;i<S.length;i++){
@@ -8,3 +9,17 @@ var numJewelsInStones = function(J, S) {
     return count
 };
 
+// O(n)time & O(n) time
+var numJewelsInStones = function(J, S) {
+    let dp = {}
+    let count = 0
+    for(let i=0;i<J.length;i++){
+        dp[J[i]] = J[i]
+    }
+    for(let i=0;i<S.length;i++){
+        if(dp[S[i]]){
+            count++
+        }
+    }
+    return count
+};
