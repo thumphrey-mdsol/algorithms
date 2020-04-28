6c65a625ac007546ccecaf31a4ed67526cf50000
github username <thumphrey9123@gmail.com>
2020-04-28 17:11:54 -0400

    optimal best case univaluedbinarytree

--- src/UnivaluedBinaryTree.js
+++ src/UnivaluedBinaryTree.js
@@ -1,4 +1,20 @@
-
+var isUnivalTree = function(root) {
+    let val = root.val
+    let output = true
+    const findVals = (root) => {
+        if(root.val!==val){
+            output = false
+        }
+        if(root.left){
+            findVals(root.left)
+        }
+        if(root.right){
+           findVals(root.right)
+           }
+    }
+    findVals(root)
+    return output
+};
 
 var isUnivalTree = function(root, val = root.val) {
     if(!root)return true
