26f2290415b0a358fa3c88196832888bef2696f6
github username <thumphrey9123@gmail.com>
2020-05-11 12:13:34 -0400

    add two numbers brute

--- src/AddTwoNumbers.js
+++ src/AddTwoNumbers.js
@@ -1,3 +1,38 @@
+// brute force/lots of logic
+var addTwoNumbers = function(l1, l2) {
+    if(!l1 && !l2) return l1
+    let newLinkedList = new ListNode()
+    let result = newLinkedList
+    let carryOver = false
+    while(l1 || l2){
+        let sum = 0
+        if(l1) sum += l1.val 
+        if(l2) sum += l2.val 
+        if(carryOver){
+          sum += 1 
+        carryOver = false   
+        }
+        console.log(newLinkedList)
+        if(sum >= 10){
+            carryOver = true 
+            sum -= 10
+            newLinkedList.val = sum 
+        } else {
+           newLinkedList.val = sum  
+        }
+        if((l1 && l1.next) || (l2 && l2.next) || carryOver){
+         newLinkedList.next = new ListNode()   
+        }
+        newLinkedList = newLinkedList.next 
+        if(l1) l1 = l1.next 
+        if(l2) l2 = l2.next 
+    }
+    if(carryOver){
+       newLinkedList.val = 1 
+    } 
+    return result
+};
+
 // best solution
 var addTwoNumbers = function(l1, l2) {
     let dummyHead = new ListNode(0)
