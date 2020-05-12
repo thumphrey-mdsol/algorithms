243ef07d645c8fdc3399840f582e871de3e84153
github username <thumphrey9123@gmail.com>
2020-05-12 11:20:38 -0400

    invert tree revision

--- src/InvertBinaryTree.js
+++ src/InvertBinaryTree.js
@@ -1,4 +1,16 @@
-
+// rough draft
+var invertTree = function(root) {
+    if(!root) return root
+    const recurr = (root) => {
+        let temp = root.left
+        root.left = root.right
+        root.right = temp
+        if(root.left) recurr(root.left)
+        if(root.right) recurr(root.right)
+    }
+    recurr(root)
+    return root
+};
 
 // Best
 var invertTree = function(root) {
