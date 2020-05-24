f8fec7285ae47f688133ecb7e09e5e7a3df0d9b6
github username <thumphrey9123@gmail.com>
2020-05-24 16:11:18 -0400

    Collecting rainwater effecient

--- src/CollectingRainwater.js
+++ src/CollectingRainwater.js
@@ -23,3 +23,22 @@ var trap = function(height) {
     }
     return result
 };
+
+
+// alternate
+var trap = function(height) {
+    let left = 0
+    let maxLeft = height[left]
+    let right = height.length-1
+    let maxRight = height[right]
+    let result = 0
+    while(left <= right) {
+        maxLeft = Math.max(maxLeft, height[left])
+        maxRight = Math.max(maxRight, height[right])
+        result += maxLeft - height[left]
+        result += maxRight - height[right]
+        if(height[left] <= height[right]) left++
+        else right--
+    }
+    return result
+};
